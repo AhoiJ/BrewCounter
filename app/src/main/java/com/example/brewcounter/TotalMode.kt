b955b4f1e9836868d6f98665469cf327b57eab62
Petri <petri.pellinen@hotmail.com>
2019-04-19 18:56:24 +0300

    Juomalistaus totaltallyyn, EI TOIMI

--- app/src/main/java/com/example/brewcounter/TotalMode.kt
+++ app/src/main/java/com/example/brewcounter/TotalMode.kt
@@ -1,12 +1,68 @@
-package com.example.brewcounter
+package com.example.android.rvtutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import com.example.brewcounter.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class TotalMode : AppCompatActivity() {
+class totalModeActivity : AppCompatActivity() {
+
+    // Initializing an empty ArrayList to be filled with animals
+    val animals: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_total_mode)
+        setContentView(R.layout.activity_main)
+
+        // Loads drinks into the ArrayList
+        addDrinks()
+
+        // Creates a vertical Layout Manager
+        rv_drink_list.layoutManager = LinearLayoutManager(this)
+
+        // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
+//        rv_animal_list.layoutManager = GridLayoutManager(this, 2)
+
+        // Access the RecyclerView Adapter and load the data into it
+        rv_drink_list.adapter = drinkAdapter(animals, this)
+
+    }
+
+    // Adds animals to the empty animals ArrayList
+    fun addAnimals() {
+        animals.add("dog")
+        animals.add("cat")
+        animals.add("owl")
+        animals.add("cheetah")
+        animals.add("raccoon")
+        animals.add("bird")
+        animals.add("snake")
+        animals.add("lizard")
+        animals.add("hamster")
+        animals.add("bear")
+        animals.add("lion")
+        animals.add("tiger")
+        animals.add("horse")
+        animals.add("frog")
+        animals.add("fish")
+        animals.add("shark")
+        animals.add("turtle")
+        animals.add("elephant")
+        animals.add("cow")
+        animals.add("beaver")
+        animals.add("bison")
+        animals.add("porcupine")
+        animals.add("rat")
+        animals.add("mouse")
+        animals.add("goose")
+        animals.add("deer")
+        animals.add("fox")
+        animals.add("moose")
+        animals.add("buffalo")
+        animals.add("monkey")
+        animals.add("penguin")
+        animals.add("parrot")
     }
 }
