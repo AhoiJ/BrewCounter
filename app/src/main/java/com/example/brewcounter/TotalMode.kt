b6ab36c70e00957b6c589b9133c87f489fa6399b
Petri <petri.pellinen@hotmail.com>
2019-04-24 16:02:15 +0300

    TotalMode, Drinks added, numbers are off

--- app/src/main/java/com/example/brewcounter/TotalMode.kt
+++ app/src/main/java/com/example/brewcounter/TotalMode.kt
@@ -8,7 +8,6 @@ import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import com.example.brewcounter.R
-import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 
 class TotalMode : AppCompatActivity() {
@@ -17,9 +16,17 @@ class TotalMode : AppCompatActivity() {
 
     var smallBeer: Int = 0
     var largeBeer: Int = 0
+    var longDrink: Int = 0
+    var cider: Int = 0
+    var wineGlass: Int = 0
+    var shot: Int = 0
 
     val strSB = "Small Beer 0,33L  "
-    val strLB = "Large Beers 0,5L  "
+    val strLB = "Large Beer 0,5L    "
+    val strLD = "Long Drink            "
+    val strC = "Cider                          "
+    val strWG = "Wine Glass           "
+    val strS = "Shot                      "
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +34,7 @@ class TotalMode : AppCompatActivity() {
 
         updateList()
 
-        saveDrinks(smallBeer, largeBeer)
+        saveDrinks(smallBeer, largeBeer, longDrink, cider, wineGlass, shot)
 
 
     }
@@ -38,7 +45,7 @@ class TotalMode : AppCompatActivity() {
 
         listView = findViewById<ListView>(R.id.totalDrinkList)
 
-        val listItems = arrayOf<String>(strSB + smallBeer, strLB + largeBeer)
+        val listItems = arrayOf<String>(strSB + smallBeer, strLB + largeBeer, strLD + longDrink, strC + cider, strWG + wineGlass, strS + shot)
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
         listView.adapter = adapter
@@ -50,14 +57,23 @@ class TotalMode : AppCompatActivity() {
 
         smallBeer = sharPref.getInt("smallBeers", smallBeer)
         largeBeer = sharPref.getInt("largeBeers", largeBeer)
+        longDrink = sharPref.getInt("longDrinks", longDrink)
+        cider = sharPref.getInt("ciders", cider)
+        wineGlass = sharPref.getInt("wineGlasses", wineGlass)
+        shot = sharPref.getInt("shots", shot)
+
     }
 
-    fun saveDrinks(sB: Int, lB : Int){
+    fun saveDrinks(sB: Int, lB : Int, lD : Int, C : Int, wG : Int, S : Int){
         val sharPref = getSharedPreferences("DrinksData", 0)
         var editor = sharPref.edit()
 
         editor.putInt("smallBeers", sB)
         editor.putInt("largeBeers", lB)
+        editor.putInt("longDrinks", lD)
+        editor.putInt("ciders", C)
+        editor.putInt("wineGlasses", wG)
+        editor.putInt("shots", S)
         editor.apply()
     }
 
